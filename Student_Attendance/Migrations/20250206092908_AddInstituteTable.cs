0f1698af040999a6fc6e07f874594d0e0f31172d
param.a <param.a@hotmail.com>
2025-02-06 15:02:58 +0530

    DB Create of Institute management

---
+++ Student_Attendance/Migrations/20250206092908_AddInstituteTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Student_Attendance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInstituteTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Institutes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ShortName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Logo = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ContactNo = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Institutes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Institutes");
+        }
+    }
+}
---
+++ Student_Attendance/ViewModels/InstituteViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Attendance.ViewModels
+{
+    public class InstituteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Institute Name is required")]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Short Name is required")]
+        [StringLength(50)]
+        public string ShortName { get; set; }
+
+        public string? Logo { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(500)]
+        public string Address { get; set; }
+
+        [StringLength(200)]
+        public string? Website { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Contact No is required")]
+        [StringLength(20)]
+        public string ContactNo { get; set; }
+    }
+}
