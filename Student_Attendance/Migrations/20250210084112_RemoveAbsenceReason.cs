52eaac077d30404e3ac8b09b3a1d52abc970e9f4
param.a <param.a@hotmail.com>
2025-02-10 14:11:37 +0530

    Migration RemoveAbsenceReason

---
+++ Student_Attendance/Migrations/20250210084112_RemoveAbsenceReason.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Student_Attendance.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAbsenceReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AbsenceReason",
+                table: "AttendanceRecords");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AbsenceReason",
+                table: "AttendanceRecords",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
