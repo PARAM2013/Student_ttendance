4674c230f100c91af8ee610c62bc2c384f95ff2a
param.a <param.a@hotmail.com>
2025-02-10 15:23:44 +0530

    Excel 3 error

--- Student_Attendance/ViewModels/StudentImportViewModel.cs
+++ Student_Attendance/ViewModels/StudentImportViewModel.cs
@@ -5,7 +5,9 @@ namespace Student_Attendance.ViewModels
 {
     public class StudentImportViewModel
     {
-        public IFormFile File { get; set; }
+        public IFormFile? File { get; set; }
         public List<string> ImportErrors { get; set; } = new List<string>();
+        public int SuccessCount { get; set; }
+        public int ErrorCount { get; set; }
     }
 }
