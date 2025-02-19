ff6ee0fede255ea9e4a4f724be1d5fe5583e5e0e
param.a <param.a@hotmail.com>
2025-02-19 16:33:05 +0530

    Fix Techer subject

---
+++ Student_Attendance/Migrations/20250219105815_Fix_TeacherSubject_AcademicYearAndIsActive.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Student_Attendance.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_TeacherSubject_AcademicYearAndIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AcademicYearId",
+                table: "TeacherSubjects",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "TeacherSubjects",
+                nullable: false,
+                defaultValue: true);
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
